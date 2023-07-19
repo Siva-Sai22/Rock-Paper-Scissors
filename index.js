adc1439b706fa15063bf23256212961b4cf7ce6b
Siva_Sai <22je0483@iitism.ac.in>
2023-07-19 20:04:55 +0530

    Complete the playground function

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
-    if(randomNumber===0) return "Rock";
-    else if(randomNumber===1) return "Paper";
-    else return "Scissor";
+    if(randomNumber===0) return "rock";
+    else if(randomNumber===1) return "paper";
+    else return "scissor";
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    playerSelection=playerSelection.toLowerCase();
+    if(playerSelection===computerSelection){
+        return "Its a draw!";
+    }else if(playerSelection==="rock" && computerSelection==="paper"){
+        return "You Lose! Paper beats Rock";
+    }else if(playerSelection==="rock" && computerSelection==="scissor"){
+        return "You Won! Rock beats Scissor";
+    }else if(playerSelection==="paper" && computerSelection==="rock"){
+        return "You Won! Paper beats Rock";
+    }else if(playerSelection==="paper" && computerSelection==="scissor"){
+        return "You Lose! Scissor beats Paper";
+    }else if(playerSelection==="scissor" && computerSelection==="paper"){
+        return "You Won! Scissor beats Paper";
+    }else if(playerSelection==="scissor" && computerSelection==="rock"){
+        return "You Lose! Scissor beats Rock";
+    }
+}
+
+const playerSelection = "Scissor";
+const computerSelection=getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
